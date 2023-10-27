25a464eaf03d72541aeb7b143b31433455e8cfba
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-27 09:20:47 +0300

    effect-ipa2222

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,9 +1,13 @@
-function ToDo(props) {
+import { useState } from "react";
 
+function ToDo(props) {
+const [check,setCheck] = useState(props.todo.completed)
   return (
     <>
         <p>{props.todo.title}</p>
-        <input type="checkbox" checkvalue={props.todo.completed}></input>
+  <p>userID: {props.todo.userId}</p>
+  <p>userId: {props.todo.id}</p>
+        <input type="checkbox" checked={check} onChange={(e) => setCheck(e.target.checked)}></input>
         
     </>
   );
