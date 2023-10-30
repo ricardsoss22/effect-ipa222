68858e10dfc5ec9b405eb1b64cc7401b763dbd30
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-30 15:21:21 +0200

    effect-ipa2222

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>hi</h1>
-      <ToDo todo={todo}/>
+      <ToDo {...todo} />
     </div>
   );
 }
--- src/ToDo.js
+++ src/ToDo.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
 function ToDo(props) {
-const [check,setCheck] = useState(props.todo.completed)
+const [check,setCheck] = useState(props.completed)
   return (
     <>
-        <p>{props.todo.title}</p>
-  <p>userID: {props.todo.userId}</p>
-  <p>userId: {props.todo.id}</p>
+        <p>{props.title}</p>
+  <p>userID: {props.userId}</p>
+  <p>userId: {props.id}</p>
         <input type="checkbox" checked={check} onChange={(e) => setCheck(e.target.checked)}></input>
         
     </>
